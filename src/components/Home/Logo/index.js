8825f1ff7569222d044feb6de829bf196d8a1f5b
unknown <Idan.barz@gmail.com>
2022-06-11 15:42:55 +0300

    123

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react'
+import gsap from 'gsap-trial'
+import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import LogoS from '../../../assets/images/logo-i.png'
+import './index.scss'
+
+const Logo = () => {
+  const bgRef = useRef()
+  const outlineLogoRef = useRef()
+  const solidLogoRef = useRef()
+
+  return (
+    <div className="logo-container" ref={bgRef}>
+      <img
+        className="solid-logo"
+        ref={solidLogoRef}
+        src={LogoS}
+        alt="JavaScript,  Developer"
+      />
+    </div>
+  )
+}
+
+export default Logo
